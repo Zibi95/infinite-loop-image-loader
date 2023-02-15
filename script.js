61a77169c89c20dc248dc097a98199751b28aa61
Zibi95 <sylwester.sob95@gmail.com>
2023-02-15 19:27:47 +0100

    Init small project that i did without github yet

---
+++ script.js
@@ -0,0 +1,58 @@
+const loader = document.querySelector('.loader');
+const images = document.querySelectorAll('img');
+const imagesContainer = document.querySelector('.images-container');
+let imagesLoaded = false;
+
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && imagesLoaded) {
+    imagesLoaded = false;
+    getImagesApiCall(10);
+  }
+});
+
+function showOrHideLoader() {
+  loader.hidden = !loader.hidden;
+}
+
+function changeImagesLoaded() {
+  imagesLoaded = !imagesLoaded;
+}
+
+function renderMarkupWithImages(images) {
+  const htmlMarkup = images
+    .map(image => {
+      return ` <a href='${image.links.html}' target='_blank''><div class="image-card">
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <div class="card-hover"></div>
+    <img class="card-contents" alt='${image.alt_description}' src="${image.urls.small}" />
+  </div></a>`;
+    })
+    .join('');
+  return insertImagesIntoDOM('beforeend', htmlMarkup);
+}
+
+function insertImagesIntoDOM(where, what) {
+  imagesContainer.insertAdjacentHTML(where, what);
+  changeImagesLoaded();
+  showOrHideLoader();
+}
+
+async function getImagesApiCall(count = 30) {
+  showOrHideLoader();
+  const apiKey = 'FvVwI8JYL2q0_faKBn9nvbw3Yri8ak32Sdl4gnM3h8Q';
+  try {
+    const response = await fetch(`https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`);
+    const images = await response.json();
+    return renderMarkupWithImages(images);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+getImagesApiCall();
